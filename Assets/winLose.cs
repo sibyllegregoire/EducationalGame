ce4b5e130fb4c11eb1a0cd1c1b776a5df6f59dd2
Sibylle Gregoire <sibylleanne.gregoire@mail.polimi.it>
2021-12-06 14:16:25 +0100

    time and score

--- Assets/winLose.cs
+++ Assets/winLose.cs
@@ -8,58 +8,228 @@ public class winLose : MonoBehaviour
 {
     public static int Score;
     public Text ScoreText;
+    public Text Message;
     public int Soft;
     public int Hard;
+    public bool isRacing;
+    public float currentTime;
+    public float endTime;
+    public float Stars;
+    Text txt;
+    public bool star1ImgOn;
+    public Image star1;
+    public bool star2ImgOn;
+    public Image star2;
+    public bool star3ImgOn;
+    public Image star3;
+
 
 	public void Awake()
 	{
 		Score = winLose.Score;
 		ScoreText.text = Score.ToString();
+        
     	//DontDestroyOnLoad(this.gameObject);
-	}
+        txt = GetComponent<Text>();
+        isRacing = true;
+        star1.enabled = false;
+        star1ImgOn = false;
+        star2.enabled = false;
+        star2ImgOn = false;
+        star3.enabled = false;
+        star3ImgOn = false;
+    }
 
     // Update is called once per frame
     public void Update()
 	{
+        if(isRacing)
+        txt.text = currentTime.ToString("#.00");
+        currentTime= currentTime+Time.deltaTime;
+        
         if(Score >= 10)
 		{
             YouWin();
+            
         }
      }
+    //void Start()
+    
+        //isRacing = true;
+    
+
+
     public void YouWin()
 	{
-        ScoreText.text = "You Win!";
+        Message.text = "You Win!";
         Time.timeScale = 0f;
+        txt = GetComponent<Text>();
+        isRacing = false;
+        Stars = Stars / Score;
+        if (Stars>0f  &&  Stars<1f)
+        {
+            Message.text = "Final Score";
+            star1.enabled = true;
+            star1ImgOn = true;
+            star2.enabled = false;
+            star2ImgOn = false;
+            star3.enabled = false;
+            star3ImgOn = false;
+        }
+        if (Stars>1f  &&  Stars<2f)
+        {
+            Message.text = "Final Score";
+            star1.enabled = true;
+            star1ImgOn = true;
+            star2.enabled = true;
+            star2ImgOn = true;
+            star3.enabled = false;
+            star3ImgOn = false;
+        }
+        if (Stars>2f  &&  Stars<3f)
+        {
+            Message.text = "Final Score";
+            star1.enabled = true;
+            star1ImgOn = true;
+            star2.enabled = true;
+            star2ImgOn = true;
+            star3.enabled = true;
+            star3ImgOn = true;
+        }
+            
     }
  
     public void SoftAddScore()
     {
+        //currentTime = 0f;
         if (Soft == 1)
         {
+            if (currentTime>0f  &&  currentTime<2f)
+            {
+                Stars = Stars+3;
+                Message.text = "You are fast as a rocket!";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = true;
+                star2ImgOn = true;
+                star3.enabled = true;
+                star3ImgOn = true;
+                
+ 
+               
+            }
+            if (currentTime>2f  &&  currentTime<4f)
+            {
+                Stars = Stars+2;
+                Message.text = "You are right";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = true;
+                star2ImgOn = true;
+                star3.enabled = false;
+                star3ImgOn = false;
+            }
+            if (currentTime>4f  &&  currentTime<5f)
+            {
+                Stars = Stars+ 1;
+                Message.text = "You are right";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = false;
+                star2ImgOn = false;
+                star3.enabled = false;
+                star3ImgOn = false;
+            }
             Score++;
             ScoreText.text = Score.ToString();
+            ScoreText.text = "You are right!";
             Debug.Log(ScoreText.text);
+            isRacing = false;
+            
+            if (currentTime >= 5f)
+            {
+                
+                Message.text = "You're too slow!";
+                Debug.Log(ScoreText.text);
+            }
+
         }
         else
         {
 			Score = Score;
-            ScoreText.text = "You are Wrong!";
+            Message.text = "You are Wrong!";
+            //txt = GetComponent<Text>();
+            isRacing = false;
+            
             //Time.timeScale = 0f;
         }
     }
     
     public void HardAddScore()
     {
+        //currentTime = 0f;
         if (Hard == 1)
         {
+            if (currentTime>0f  &&  currentTime<2f)
+            {
+                Stars = Stars+3;
+                Message.text = "You are fast as a rocket!";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = true;
+                star2ImgOn = true;
+                star3.enabled = true;
+                star3ImgOn = true;
+                
+ 
+               
+            }
+            if (currentTime>2f  &&  currentTime<4f)
+            {
+                Stars = Stars+2;
+                Message.text = "You are right";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = true;
+                star2ImgOn = true;
+                star3.enabled = false;
+                star3ImgOn = false;
+            }
+            if (currentTime>4f  &&  currentTime<5f)
+            {
+                Stars = Stars+1;
+                Message.text = "You are right";
+                star1.enabled = true;
+                star1ImgOn = true;
+                star2.enabled = false;
+                star2ImgOn = false;
+                star3.enabled = false;
+                star3ImgOn = false;
+            }
+            
             Score++;
+            
             ScoreText.text = Score.ToString();
+            
             Debug.Log(ScoreText.text);
+            isRacing = false;
+            //endTime = currentTime;
+            if (currentTime >= 5f)
+            {
+                Message.text = "You're too slow!";
+                Debug.Log(ScoreText.text);
+            }
+            currentTime = 0f;
         }
         else
         {
 			//Score = Score;
-            ScoreText.text = "You are Wrong!";
+            Message.text = "You are Wrong!";
+            isRacing = false;
+            currentTime = 0f;
+
+
+
             //Time.timeScale = 0f;
         }
     }
@@ -67,10 +237,14 @@ public class winLose : MonoBehaviour
     public void changescene()
     {
         SceneManager.LoadScene("Test");
+        
     }
 
     public void changescene2()
     {
         SceneManager.LoadScene("SampleScene");
+        
     }
+
+
 }
